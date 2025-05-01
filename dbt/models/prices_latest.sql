20dd413cfe98edb7b0739563cdabac4a9df3c8b1
Jake Brehm <jake.m.brehm@gmail.com>
2025-05-01 17:13:46 +0000

    Fix duplicate rows in prices_latest model

--- dbt/models/prices_latest.sql
+++ dbt/models/prices_latest.sql
@@ -1,6 +1,7 @@
 {{ config(alias=env_var("DBT_BQ_VIEW_PRICES_LATEST")) }}
 
 with
+
     ranked_rows as (
         select
             uuid,
@@ -11,9 +12,15 @@ with
                 partition by item_id order by recorded_at desc
             ) as row_num
         from {{ var("prices_table") }}
+    ),
+
+    latest_prices as (
+        select i.id, r.uuid, r.price, i.name, i.description, r.recorded_at
+        from ranked_rows r
+        inner join {{ var("items_table") }} i on i.id = r.id
+        where r.row_num = 1
     )
 
-select i.id, r.uuid, r.price, i.name, i.description, r.recorded_at
-from ranked_rows r
-inner join {{ var("items_table") }} i on i.id = r.id
-where r.row_num = 1
+select *
+from latest_prices
+group by id, uuid, price, name, description, recorded_at
